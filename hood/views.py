b8a0c38f23ff5a681f40e7a1d2f90d2c73209e2b
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-20 15:30:39 +0300

    create a view function for the index page.

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    '''View function to return the index page and all of its data'''
+    
+    return render(request, 'index.html')
+    
