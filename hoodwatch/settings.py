22cba83e197acfe3a4a5af464fd61ac58de756b2
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-20 16:57:27 +0300

    added cloudinary configurations.

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +134,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+cloudinary.config( 
+  cloud_name = "dzc2fvt9a",  
+  api_key = "469381447855196", 
+  api_secret = "O65PpSAfpkrux_nRTg6Pqbe-7NA" 
+)
